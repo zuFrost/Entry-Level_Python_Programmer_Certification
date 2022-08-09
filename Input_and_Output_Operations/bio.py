49df8285a707165e569b1718bfda05cd9b31b1de
Alex Frost <zufrost.a@gmail.com>
2022-08-09 23:26:11 +0000

    add 2nd variant

--- Input_and_Output_Operations/bio.py
+++ Input_and_Output_Operations/bio.py
@@ -1,3 +1,15 @@
 name = input("What is your name? ")
 color = input("What is your favorit color? ")
 age = int(input("Haw old are you today? "))
+
+#NAME is AGE years old and loves the color COLOR.
+
+#1st variant
+# print(name)
+# print("is " + str(age) + " years old")
+# print("and loves the color " + color + ".")
+
+#2st variant
+print(name, end=" ")
+print("is " + str(age) + " years old", end=" ")
+print("and loves the color " + color + ".", end=" ")
